be79427c3433460c2ad385e9a3f7de2bff2fdc07
Harry Mahardhika <harrymahardhika@gmail.com>
2024-09-20 03:01:59 +0700

    added config for i3wm, polybar, and picom

---
+++ .config/i3/xinput-settings.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TOUCHPAD="SynPS/2 Synaptics TouchPad"
+xinput set-prop "$TOUCHPAD" "libinput Tapping Enabled" 1
+xinput set-prop "$TOUCHPAD" "libinput Natural Scrolling Enabled" 1
+xinput set-prop "$TOUCHPAD" "libinput Accel Speed" 0.5
+
+# Enable two-finger scrolling
+# Note: "libinput Scroll Method Enabled" takes an array of values
+# (0=two-finger, 1=edge, 2=on-button down)
+xinput set-prop "$TOUCHPAD" "libinput Scroll Method Enabled" 0, 0, 1
+
+MOUSE="SynPS/2 Synaptics TouchPad"
+xinput set-prop "$MOUSE" "libinput Natural Scrolling Enabled" 1
+xinput set-prop "$MOUSE" "libinput Accel Speed" 0.5
+
---
+++ .config/polybar/launch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+polybar-msg cmd quit
+
+echo "---" | tee -a /tmp/polybar.log
+polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+
+echo "Polybar launched..."
