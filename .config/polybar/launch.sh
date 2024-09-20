59d9faebfe39cf4bf0f18bca28eabd16b0b13b53
Harry Mahardhika <harrymahardhika@gmail.com>
2024-09-20 16:16:14 +0700

    fix polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
-polybar-msg cmd quit
+# Terminate already running bar instances
+killall -q polybar
 
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Detect active monitors
+if xrandr | grep -q "HDMI-2 connected"; then
+  MONITOR=HDMI-2
+elif xrandr | grep -q "HDMI-1 connected"; then
+  MONITOR=HDMI-1
+else
+  MONITOR=eDP-1
+fi
+
+# Launch Polybar with the detected monitor
 echo "---" | tee -a /tmp/polybar.log
-polybar main --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
+MONITOR=$MONITOR polybar main --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
 
-echo "Polybar launched..."
