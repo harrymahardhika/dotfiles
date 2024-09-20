e634c514c3c22fe83f43bdf5cb3fea97d0ad488c
Harry Mahardhika <harrymahardhika@gmail.com>
2024-09-21 00:17:45 +0700

    updates
    - configure i3, polybar, and picom
    - update lualine config
    - change starship icon
    - update tmux config

--- .config/i3/xinput-settings.sh
+++ .config/i3/xinput-settings.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+MOUSE="pointer:Logitech Wireless Mouse"
+xinput set-prop "$MOUSE" "libinput Natural Scrolling Enabled" 1
+xinput set-prop "$MOUSE" "libinput Accel Speed" 0.5
+
 TOUCHPAD="SynPS/2 Synaptics TouchPad"
 xinput set-prop "$TOUCHPAD" "libinput Tapping Enabled" 1
 xinput set-prop "$TOUCHPAD" "libinput Natural Scrolling Enabled" 1
@@ -9,8 +13,4 @@ xinput set-prop "$TOUCHPAD" "libinput Accel Speed" 0.5
 # Note: "libinput Scroll Method Enabled" takes an array of values
 # (0=two-finger, 1=edge, 2=on-button down)
 xinput set-prop "$TOUCHPAD" "libinput Scroll Method Enabled" 0, 0, 1
-
-MOUSE="SynPS/2 Synaptics TouchPad"
-xinput set-prop "$MOUSE" "libinput Natural Scrolling Enabled" 1
-xinput set-prop "$MOUSE" "libinput Accel Speed" 0.5
 
