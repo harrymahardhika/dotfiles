f5e2e202a045f5fb831d26b44f472bb4dc3bb0e1
Harry Mahardhika <harrymahardhika@gmail.com>
2024-09-22 17:56:33 +0700

    updates
    - change kitty font size
    - tree-sitter add vue
    - tmux: add tmux-resurrect

--- .config/nvim/lua/harry/plugins/treesitter.lua
+++ .config/nvim/lua/harry/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     local config = require("nvim-treesitter.configs")
 
     config.setup({
-      ensure_installed = { "lua", "php", "typescript", "javascript" },
+      ensure_installed = { "lua", "php", "typescript", "javascript", "vue" },
       sync_install = false,
       highlight = {
         enable = true,
