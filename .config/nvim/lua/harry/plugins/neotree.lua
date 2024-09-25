7d7df652a60dc3d1378e04c3e13aba36c0b0f5ec
Harry Mahardhika <harrymahardhika@gmail.com>
2024-09-25 19:50:05 +0700

    Update Neovim plugins and configuration
    - Added `neo-tree.nvim` plugin with right window position in Neovim configuration.
    - Updated `nvim-lspconfig` and `nvim-treesitter` plugin versions in `lazy-lock.json`.
    - Adjusted blur strength from 7 to 3 in `picom.conf` for smoother visuals.

---
+++ .config/nvim/lua/harry/plugins/neotree.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      log_level = "warn",
+      window = {
+        position = "right"
+      }
+    })
+  end
+}
