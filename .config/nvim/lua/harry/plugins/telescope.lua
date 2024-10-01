4b5d3b9cdf017dc5ded6133a91240de59006308c
Harry Mahardhika <harrymahardhika@gmail.com>
2024-10-01 21:16:29 +0700

    Add multi-cursor, text-case, and which-key plugins
    - Added `vim-visual-multi` plugin for multi-cursor support.
    - Added `text-case.nvim` plugin for changing text case with Telescope integration.
    - Added placeholder for `which-key.nvim` configuration but commented it out.
    - Updated Telescope keybindings with descriptions.
    - Updated `lazygit.nvim` and other dependencies in `lazy-lock.json`.

--- .config/nvim/lua/harry/plugins/telescope.lua
+++ .config/nvim/lua/harry/plugins/telescope.lua
@@ -1,18 +1,18 @@
 return {
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      require('telescope').load_extension('fzf')
+      require("telescope").load_extension("fzf")
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>fe", builtin.buffers, {})
-      vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {})
-      vim.keymap.set("n", "<leader>fp", builtin.git_files, {})
-      vim.keymap.set("n", "<leader><leader>", builtin.git_files, {})
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
+      vim.keymap.set("n", "<leader>fe", builtin.buffers, { desc = "Buffers" })
+      vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Old Files" })
+      vim.keymap.set("n", "<leader>fp", builtin.git_files, { desc = "Git Files" })
+      vim.keymap.set("n", "<leader><leader>", builtin.git_files, { desc = "Git Files" })
     end
   }
 }
