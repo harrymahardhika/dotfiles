85f6b98607aab3bbc28dd8c7e73e75d0a4b15681
Harry Mahardhika <harrymahardhika@gmail.com>
2024-10-02 17:19:17 +0700

    Enhance i3, keyd, and picom configurations
    - Updated i3 config with custom font for dmenu and added screenshot shortcut (Shift + Mod + P).
    - Added arrow key remapping layer in keyd config triggered by the SysRq key.
    - Modified picom launch script to use a simple sleep instead of a loop for shutting down processes.
    - Updated lazy-lock.json with newer commits for `lazygit.nvim`, `nvim-lspconfig`, and others.

--- .config/picom/launch.sh
+++ .config/picom/launch.sh
@@ -13,7 +13,8 @@ get_os() {
 killall -q picom
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
+# while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
+sleep 1
 
 OS=$(get_os)
 
