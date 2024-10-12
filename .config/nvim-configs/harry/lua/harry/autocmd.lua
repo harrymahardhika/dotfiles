4563543beb601f5e2654ebdebe1e5fb5db43ebdb
Harry Mahardhika <harrymahardhika@gmail.com>
2024-10-12 12:06:01 +0700

    fix auto open telescope on nvim start

--- .config/nvim-configs/harry/lua/harry/autocmd.lua
+++ .config/nvim-configs/harry/lua/harry/autocmd.lua
@@ -15,19 +15,22 @@ vim.api.nvim_create_autocmd("VimEnter", {
     -- Get the first argument passed to Neovim
     local arg = vim.fn.argv(0)
 
-    -- If a file is passed as an argument (e.g., during `git commit`), do nothing
-    -- if arg ~= "" then
-    --   return
-    -- end
+    -- If a file is passed as an argument, check if it's a git commit (skip telescope in this case)
+    if arg:match("COMMIT_EDITMSG") then
+      return
+    end
 
-    -- Check if the current directory is a Git repository
-    local is_git_repo = vim.fn.trim(vim.fn.system("git rev-parse --is-inside-work-tree")) == "true"
+    -- If no argument is passed or the argument is a directory, proceed with Telescope
+    if arg == "" or vim.fn.isdirectory(arg) == 1 then
+      -- Check if the current directory is a Git repository
+      local is_git_repo = vim.fn.trim(vim.fn.system("git rev-parse --is-inside-work-tree")) == "true"
 
-    -- If the directory is a Git repo, use git_files picker, otherwise use find_files
-    if is_git_repo then
-      require("telescope.builtin").git_files()
-    else
-      require("telescope.builtin").find_files()
+      -- If the directory is a Git repo, use git_files picker, otherwise use find_files
+      if is_git_repo then
+        require("telescope.builtin").git_files()
+      else
+        require("telescope.builtin").find_files()
+      end
     end
   end
 })
