2696dd39af043449bdfa415365ce1f03759ec7e1
Harry Mahardhika <harrymahardhika@gmail.com>
2024-10-23 11:49:47 +0700

    add neogit

--- nvim-configs/harry/lua/harry/plugins/git.lua
+++ nvim-configs/harry/lua/harry/plugins/git.lua
@@ -120,5 +120,19 @@ return {
         end
       })
     end
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",  -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      -- "ibhagwan/fzf-lua",            -- optional
+      -- "echasnovski/mini.pick",       -- optional
+    },
+    config = true
   }
+
 }
