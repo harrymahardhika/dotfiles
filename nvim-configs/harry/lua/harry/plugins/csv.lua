ce86c847cdddb25cf347ff28132960a9ee8ddfbe
Harry Mahardhika <harrymahardhika@gmail.com>
2025-01-26 09:02:29 +0700

    add rainbow_csv plugin and update lsp configuration
    - add rainbow_csv plugin with setup and key mappings for csv operations
    - include file type and command configurations for rainbow_csv plugin
    - add lua language server configuration with workspace and runtime settings
    - set lua diagnostics to recognize global 'vim' in lsp settings

---
+++ nvim-configs/harry/lua/harry/plugins/csv.lua
@@ -0,0 +1,23 @@
+return {
+  'cameron-wags/rainbow_csv.nvim',
+  config = function()
+    require('rainbow_csv').setup()
+    vim.keymap.set('n', '<leader>ra', ':RainbowAlign<CR>')
+    vim.keymap.set('n', '<leader>rs', ':RainbowShrink<CR>')
+  end,
+  ft = {
+    'csv',
+    'tsv',
+    'csv_semicolon',
+    'csv_whitespace',
+    'csv_pipe',
+    'rfc_csv',
+    'rfc_semicolon'
+  },
+  cmd = {
+    'RainbowDelim',
+    'RainbowDelimSimple',
+    'RainbowDelimQuoted',
+    'RainbowMultiDelim'
+  }
+}
