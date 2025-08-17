848788a008f61c3a154bae38407b307ea5000553
Harry Mahardhika <harrymahardhika@gmail.com>
2025-08-17 08:08:53 +0700

    clean up nvim plugins

--- nvim-configs/beta/lua/plugins/colors.lua
+++ nvim-configs/beta/lua/plugins/colors.lua
@@ -1,60 +1,38 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-        transparent_background = true,
-        float = {
-          transparent = true,
-          solid = false,
-        },
-        no_italic = true,
-        integrations = {
-          blink_cmp = {
-            style = "bordered",
-          },
-          cmp = true,
-          copilot_vim = true,
-          flash = true,
-          gitsigns = true,
-          grug_far = true,
-          mini = {
-            enabled = true,
-            indentscope_color = "surface0",
-          },
-          noice = true,
-          notify = true,
-          nvimtree = true,
-          neotree = true,
-          treesitter = true,
-          which_key = true,
-        },
-      })
-
-      vim.cmd("colorscheme catppuccin")
-    end,
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("tokyonight").setup({
-        style = "moon",
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "mocha",
+      transparent_background = true,
+      float = {
         transparent = true,
-        terminal_colors = true,
-        styles = {
-          comments = { italic = false },
-          keywords = { italic = false },
-          functions = { italic = false },
-          variables = { italic = false },
+        solid = false,
+      },
+      no_italic = true,
+      integrations = {
+        blink_cmp = {
+          style = "bordered",
         },
-      })
+        cmp = true,
+        copilot_vim = true,
+        flash = true,
+        gitsigns = true,
+        grug_far = true,
+        mini = {
+          enabled = true,
+          indentscope_color = "surface0",
+        },
+        noice = true,
+        notify = true,
+        nvimtree = true,
+        neotree = true,
+        treesitter = true,
+        which_key = true,
+      },
+    })
 
-      -- vim.cmd("colorscheme tokyonight")
-    end,
-  },
+    vim.cmd("colorscheme catppuccin")
+  end,
 }
--- nvim-configs/beta/lua/plugins/git.lua
+++ nvim-configs/beta/lua/plugins/git.lua
@@ -58,26 +58,6 @@ return {
     end,
   },
   {
-    "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with "keys" is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-    },
-  },
-  {
     "NeogitOrg/neogit",
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
