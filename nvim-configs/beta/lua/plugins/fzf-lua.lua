d21a2d262b3f4793e48c7c6b0d26e3d523192916
Harry Mahardhika <harrymahardhika@gmail.com>
2025-08-20 07:00:06 +0700

    feat(dev): Replace Telescope with fzf-lua for fuzzy finding
    - Added fzf-lua plugin with custom keymaps mirroring Telescope usage
    - Commented out Telescope config for optional fallback
    - Enhanced preview layout and visual consistency with fzf-lua setup
    - Tweaked tmux keybindings and formatting for improved UX

---
+++ nvim-configs/beta/lua/plugins/fzf-lua.lua
@@ -0,0 +1,56 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional for icons
+    config = function()
+      local fzf = require("fzf-lua")
+
+      fzf.setup({
+        winopts = {
+          border = "rounded",
+          preview = {
+            layout = "flex",
+            scrollbar = false,
+          },
+        },
+      })
+
+      -- Keymaps (mirroring your Telescope setup)
+      vim.keymap.set("n", "<leader><space>", function()
+        require("fzf-lua").files({ hidden = true }) -- no cwd field
+      end, { desc = "Find Files" })
+
+      vim.keymap.set("n", "<leader>ff", function()
+        require("fzf-lua").files({ hidden = true })
+      end, { desc = "Find Files" })
+
+      vim.keymap.set("n", "<leader>,", fzf.buffers, { desc = "Buffers" })
+      vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "Buffers" })
+      vim.keymap.set("n", "<leader>.", fzf.live_grep, { desc = "Live Grep" })
+      vim.keymap.set("n", "<leader>fg", fzf.git_files, { desc = "Git Files" })
+      vim.keymap.set("n", "<leader>f/", fzf.blines, { desc = "Fuzzy Find in Buffer" })
+      vim.keymap.set("n", "<leader>fr", fzf.oldfiles, { desc = "Recent Files" })
+
+      vim.keymap.set("n", "<leader>C", fzf.commands, { desc = "Commands" })
+      vim.keymap.set("n", "<leader>:", fzf.command_history, { desc = "Command History" })
+      vim.keymap.set("n", "<leader>r", fzf.registers, { desc = "Registers" })
+      vim.keymap.set("n", "<leader>fh", fzf.help_tags, { desc = "Help Tags" })
+
+      vim.keymap.set("n", "<leader>gb", fzf.git_branches, { desc = "Git Branches" })
+      vim.keymap.set("n", "<leader>gc", fzf.git_commits, { desc = "Git Commits" })
+      vim.keymap.set("n", "<leader>gs", fzf.git_status, { desc = "Git Status" })
+      vim.keymap.set("n", "<leader>gS", fzf.git_stash, { desc = "Git Stash" })
+      vim.keymap.set("n", "<leader>gC", fzf.git_bcommits, { desc = "Git Buffer Commits" })
+
+      vim.keymap.set("n", "<leader>sm", fzf.marks, { desc = "Marks" })
+      vim.keymap.set("n", "<leader>sM", fzf.manpages, { desc = "Man Pages" })
+      vim.keymap.set("n", "<leader>st", fzf.tags, { desc = "Tags" })
+      vim.keymap.set("n", "<leader>sd", fzf.diagnostics_workspace, { desc = "Diagnostics" })
+
+      vim.keymap.set("n", "gd", fzf.lsp_typedefs, { desc = "LSP Type Definitions" })
+      vim.keymap.set("n", "gi", fzf.lsp_implementations, { desc = "LSP Implementations" })
+      vim.keymap.set("n", "gR", fzf.lsp_references, { desc = "LSP References" })
+      vim.keymap.set("n", "<leader>fd", fzf.lsp_document_symbols, { desc = "LSP Document Symbols" })
+    end,
+  },
+}
