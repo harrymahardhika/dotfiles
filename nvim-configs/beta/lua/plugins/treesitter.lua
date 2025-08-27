2dc4227937d12fe66636412bbc396d81f29dbed7
Harry Mahardhika <harrymahardhika@gmail.com>
2025-08-27 10:45:34 +0700

    chore(dev): update personal dev environment configurations
    - Change Ghostty font to JetBrainsMono Nerd Font
    - Enhance tmux status bar with date and time display
    - Expand Treesitter config with cleaner formatting and full parser list

--- nvim-configs/beta/lua/plugins/treesitter.lua
+++ nvim-configs/beta/lua/plugins/treesitter.lua
@@ -7,13 +7,28 @@ return {
       local config = require("nvim-treesitter.configs")
       config.setup({
         auto_install = false,
-        ensure_installed = { "bash", "javascript", "lua", "markdown", "php", "python", "regex", "typescript", "vue" },
-        highlight = { enable = true, additional_vim_regex_highlighting = false },
+        ensure_installed = {
+          "bash",
+          "javascript",
+          "lua",
+          "markdown",
+          "php",
+          "python",
+          "regex",
+          "typescript",
+          "vue",
+        },
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
         ignore_install = {},
-        indent = { enable = true, },
+        indent = {
+          enable = true,
+        },
         modules = {},
         sync_install = false,
       })
-    end
+    end,
   },
 }
