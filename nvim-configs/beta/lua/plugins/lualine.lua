b1d7be9148d0a1a17d1e7fd9c034cebea0f1c5c9
Harry Mahardhika <harrymahardhika@gmail.com>
2025-10-17 14:28:11 +0700

    Update gitmux config with Catppuccin colors and modern icons

--- nvim-configs/beta/lua/plugins/lualine.lua
+++ nvim-configs/beta/lua/plugins/lualine.lua
@@ -1,33 +1,33 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("lualine").setup({
-      options = {
-        component_separators = "",
-        icons_enabled = true,
-        section_separators = { left = "", right = "" },
-        theme = "catppuccin",
-      },
-      sections = {
-        lualine_a = {},
-        lualine_b = { "branch", "diff" },
-        lualine_c = { "diagnostics", "filename" },
-        lualine_x = { "filetype", "location" },
-        lualine_y = {},
-        lualine_z = {},
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
-      },
-    })
-  end,
+  -- "nvim-lualine/lualine.nvim",
+  -- event = "VeryLazy",
+  -- dependencies = {
+  --   "nvim-tree/nvim-web-devicons",
+  -- },
+  -- config = function()
+  --   require("lualine").setup({
+  --     options = {
+  --       component_separators = "",
+  --       icons_enabled = true,
+  --       section_separators = { left = "", right = "" },
+  --       theme = "catppuccin",
+  --     },
+  --     sections = {
+  --       lualine_a = {},
+  --       lualine_b = { "branch", "diff" },
+  --       lualine_c = { "diagnostics", "filename" },
+  --       lualine_x = { "filetype", "location" },
+  --       lualine_y = {},
+  --       lualine_z = {},
+  --     },
+  --     inactive_sections = {
+  --       lualine_a = {},
+  --       lualine_b = {},
+  --       lualine_c = { "filename" },
+  --       lualine_x = {},
+  --       lualine_y = {},
+  --       lualine_z = {},
+  --     },
+  --   })
+  -- end,
 }
